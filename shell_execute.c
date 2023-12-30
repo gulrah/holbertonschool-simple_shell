1c1b29282d23c038799917ed23a099eb7f36532c
gulrah <gulnarrahimli@mail.ru>
2023-12-30 10:56:47 -0800

    bettystyle_executefile

--- shell_execute.c
+++ shell_execute.c
@@ -18,7 +18,8 @@ int shell_execute(char **args)
 pid_t pid;
 int status;
 
-if (args[0] != NULL && strcmp(args[0], "exit") == 0) {
+if (args[0] != NULL && strcmp(args[0], "exit") == 0)
+{
 exit(0);
 }
 
@@ -29,13 +30,15 @@ perror("shell");
 }
 exit(EXIT_FAILURE);
 }
-else if (pid < 0) {
+else if (pid < 0)
+{
 perror("shell");
 }
-else {
+else
+{
 do {
 waitpid(pid, &status, WUNTRACED);
 } while (!WIFEXITED(status) && !WIFSIGNALED(status));
 }
-return 1;
+return (1);
 }
