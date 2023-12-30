9f2e7773af67e38719041000efc79918dd4920a0
gulrah <gulnarrahimli@mail.ru>
2023-12-30 11:14:34 -0800

    bettystyle_split.c

--- shell_split_line.c
+++ shell_split_line.c
@@ -15,32 +15,31 @@
 
 char **shell_split_line(char *line)
 {
-    int bufsize = 64;
-    int position = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token;
+int bufsize = 64;
+int position = 0;
+char **tokens = malloc(bufsize * sizeof(char *));
+char *token;
 
-    if (!tokens) {
-        fprintf(stderr, "shell: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
+if (!tokens) {
+fprintf(stderr, "shell: allocation error\n");
+exit(EXIT_FAILURE);
+}
 
-    token = strtok(line, TOK_DELIM);
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
+token = strtok(line, TOK_DELIM);
+while (token != NULL) {
+tokens[position] = token;
+position++;
+if(position >= bufsize) {
+bufsize += 64;
+tokens = realloc(tokens, bufsize * sizeof(char *));
+if (!tokens) {
+fprintf(stderr, "shell: allocation error\n");
+exit(EXIT_FAILURE);
+}
+}
 
-        if (position >= bufsize) {
-            bufsize += 64;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens) {
-                fprintf(stderr, "shell: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL, TOK_DELIM);
-    }
-    tokens[position] = NULL;
-    return tokens;
+token = strtok(NULL, TOK_DELIM);
+}
+tokens[position] = NULL;
+return (tokens);
 }
