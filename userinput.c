9abe742e560236ab02cd12b333b97b5d68070988
fammad <fred.mmov@gmail.com>
2023-12-29 23:07:49 -0800

    Edited userinput

--- userinput.c
+++ userinput.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
-char *get_user_input(void) {
-    char *buffer = NULL;
-    size_t buffer_size = 0;
-    ssize_t read_size = getline(&buffer, &buffer_size, stdin);
+	char *get_user_input(void) {
+	char *buffer = NULL;
+	size_t buffer_size = 0;
+	ssize_t read_size = getline(&buffer, &buffer_size, stdin);
 
-    if (read_size == -1) {
-        free(buffer);
-        return NULL;
-    }
+	if (read_size == -1)
+	{
+		free(buffer);
+		return NULL;
+	}
 
-    if (buffer[read_size - 1] == '\n') {
-        buffer[read_size - 1] = '\0';
-    }
+	if (buffer[read_size - 1] == '\n')
+	{
+		buffer[read_size - 1] = '\0';
+	}
 
-    return buffer;
+	return buffer;
 }
