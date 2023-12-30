1cd1ea850c947b1661870bd5568a54907f56ed35
fammad <fred.mmov@gmail.com>
2023-12-29 20:09:00 -0800

    Edited Simple loop for return

--- main_shell.c
+++ main_shell.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 int shell_loop(void) {
-    int pipe_status = !isatty(fileno(stdin));
-    char *input;
+	int pipe_status = !isatty(fileno(stdin));
+	char *input;
 
-    while (1) {
-        if (!pipe_status) {
-            printf("#shell$ ");
-            fflush(stdout);
+	while (1) {
+	if (!pipe_status) {
+		printf("#shell$ ");
+		fflush(stdout);
         }
 
-        input = get_user_input();
-        if (input == NULL) {
-            break;
-        }
+	input = get_user_input();
+	if (input == NULL) {
+		break;
+	}
 
-        if (strcmp(input, "exit") == 0) {
-            free(input);
-            exit(0);
-        }
+	if (strcmp(input, "exit") == 0) {
+		free(input);
+		exit(0);
+	}
 
-        int status = NULL;
+	int status = NULL;
 	status = handle_input(input);
 
-        if (status == 2 && pipe_status) {
-            exit(2);
-        }
-    }
+	if (status == 2 && pipe_status) {
+	exit(2);
+	}
+	}
 
     return 0;
 }
