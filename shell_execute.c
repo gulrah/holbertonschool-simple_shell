68abf099b34c7835675c0b6175a809454337e94e
gulrah <gulnarrahimli@mail.ru>
2023-12-30 07:13:38 -0800

    corrected

--- shell_execute.c
+++ shell_execute.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
 int shell_execute(char **args)
 {
-    pid_t pid;
+
+pid_t pid;
     int status;
 
-     if (args[0] != NULL && strcmp(args[0], "exit") == 0) {
+    if (args[0] != NULL && strcmp(args[0], "exit") == 0) {
         exit(0);
     }
 
@@ -13,18 +13,14 @@ int shell_execute(char **args)
     if (pid == 0) {
         if (execvp(args[0], args) == -1) {
             perror("shell");
-	     exit(2);
-	}
-            exit(EXIT_FAILURE); 
-            } else if (pid < 0) {
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
         perror("shell");
     } else {
-  
-        waitpid(pid, &status, WUNTRACED);
-        if (WIFEXITED(status)) {
-            return WEXITSTATUS(status);
-        }
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
-
     return 1;
 }
