059da025412e84ce010d3b187e184e9a0ca879a1
gulrah <gulnarrahimli@mail.ru>
2023-12-30 06:37:58 -0800

    edited_executefile

--- shell_execute.c
+++ shell_execute.c
@@ -5,18 +5,25 @@ int shell_execute(char **args)
     pid_t pid;
     int status;
 
+     if (args[0] != NULL && strcmp(args[0], "exit") == 0) {
+        exit(0);
+    }
+
     pid = fork();
     if (pid == 0) {
         if (execvp(args[0], args) == -1) {
             perror("shell");
+	     exit(2);
+	}
             exit(EXIT_FAILURE); 
-        }
-    } else if (pid < 0) {
+            } else if (pid < 0) {
         perror("shell");
     } else {
-        do {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  
+        waitpid(pid, &status, WUNTRACED);
+        if (WIFEXITED(status)) {
+            return WEXITSTATUS(status);
+        }
     }
 
     return 1;
