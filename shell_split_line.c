e133c83c9766668ef95b13503103c436e17a20eb
gulrah <gulnarrahimli@mail.ru>
2023-12-30 11:17:10 -0800

    bettystyle_split.c

--- shell_split_line.c
+++ shell_split_line.c
@@ -20,19 +20,23 @@ int position = 0;
 char **tokens = malloc(bufsize * sizeof(char *));
 char *token;
 
-if (!tokens) {
+if (!tokens)
+{
 fprintf(stderr, "shell: allocation error\n");
 exit(EXIT_FAILURE);
 }
 
 token = strtok(line, TOK_DELIM);
-while (token != NULL) {
+while (token != NULL)
+{
 tokens[position] = token;
 position++;
-if(position >= bufsize) {
+if(position >= bufsize)
+{
 bufsize += 64;
-tokens = realloc(tokens, bufsize * sizeof(char *));
-if (!tokens) {
+tokens = realloc(tokens, bufsize *sizeof(char *));
+if (!tokens)
+{
 fprintf(stderr, "shell: allocation error\n");
 exit(EXIT_FAILURE);
 }
